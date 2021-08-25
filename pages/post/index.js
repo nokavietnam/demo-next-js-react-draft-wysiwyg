7ee0d1b6b317810480efbde81f8b650bcca6eab3
noka <noka.hient@gmail.com>
2021-08-25 16:15:56 +0700

    add rich text editor

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 import dynamic from "next/dynamic";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+const EditorState = dynamic(
+  import("draft-js").then((mode) => mode.EditorState),
+  {
+    ssr: false,
+    loading: function loading() {
+      return <div>Lodding.....</div>;
+    },
+  }
+);
+
+const Editor = dynamic(
+  import("react-draft-wysiwyg").then((mod) => mod.Editor),
+  {
+    ssr: false,
+    loading: function loading() {
+      return <div>Loading......</div>;
+    },
+  }
+);
 
 const Post = () => {
-  return <div></div>;
+  const [editorState, setEditorState] = useState(EditorState.createEmpty);
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+  };
+
+  return (
+    <div className="">
+      <Editor
+        editorState={editorState}
+        wrapperClassName="demo-wrapper"
+        editorClassName="demo-editor"
+        onEditorStateChange={onEditorStateChange}
+      />
+    </div>
+  );
 };
 
 export default Post;
